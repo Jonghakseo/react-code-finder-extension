9f4bb8dd712524105601e96aa521f8ee8f10aee7
JongHak Seo <unqocn@gmail.com>
2024-02-14 18:30:39 +0900

    fix: delete unused permission

--- manifest.js
+++ manifest.js
@@ -15,7 +15,7 @@ const manifest = {
   name: '__MSG_extensionName__',
   version: packageJson.version,
   description: '__MSG_extensionDescription__',
-  permissions: ['storage', 'sidePanel', 'scripting'],
+  permissions: ['storage', 'sidePanel'],
   host_permissions: ['<all_urls>'],
   side_panel: {
     default_path: 'src/pages/sidepanel/index.html',
